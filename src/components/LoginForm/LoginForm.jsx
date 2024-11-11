358332b7c02062935dcc5c45cc3486f195a28f55
Mykhailo Konstantinov <mkonst10@gmail.com>
2024-11-11 18:29:06 +0200

    add login form

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,63 @@
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { useDispatch } from "react-redux";
+import * as Yup from "yup";
+import styles from "./LoginForm.module.css";
+import { login } from "../../redux/auth/operations";
+
+const loginUserSchema = Yup.object({
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password length must be at least 7 characters")
+    .max(15, "Password must be less than 15 characters")
+    .required("Password is required"),
+});
+
+const initialValues = {
+  email: "",
+  password: "",
+};
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, actions) => {
+    dispatch(login(values));
+    actions.resetForm();
+  };
+
+  return (
+    <div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={loginUserSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form className={styles.form}>
+          <label className={styles.label}>
+            <span className={styles.inputName}>Email</span>
+            <Field className={styles.field} type="text" name="email" />
+            <ErrorMessage
+              className={styles.error}
+              name="email"
+              component="span"
+            />
+          </label>
+          <label className={styles.label}>
+            <span className={styles.inputName}>Password</span>
+            <Field className={styles.field} type="password" name="password" />
+            <ErrorMessage
+              className={styles.error}
+              name="password"
+              component="span"
+            />
+          </label>
+          <button type="submit">Sign In</button>
+        </Form>
+      </Formik>
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { login, register } from "./operations";
 
 const INITIAL_STATE = {
   user: {
@@ -8,12 +9,46 @@ const INITIAL_STATE = {
   token: null,
   isLoggedIn: false,
   isRefreshing: false,
+  isLoading: false,
+  error: null,
 };
 
 export const authSlice = createSlice({
   name: "auth",
   initialState: INITIAL_STATE,
-  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isLoggedIn = true;
+        state.token = action.payload.token;
+        state.user.name = action.payload.user.name;
+        state.user.email = action.payload.user.email;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      })
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isLoggedIn = true;
+        state.token = action.payload.token;
+        state.user.name = action.payload.user.name;
+        state.user.email = action.payload.user.email;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export const authReducer = authSlice.reducer;
